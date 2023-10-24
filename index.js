6f5032197da47e862a23ddbf3d8cf21e34165781
shmiki <shmiki@microsoft.com>
2023-10-24 14:48:32 +0900

    Seach Query を塚

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const msalConfig  = {
       clientSecret: process.env.CLIENT_SECRET, 
     },
   };
+
 // ユーザ情報を取得する関数
 async function getUserDetails() {
   const cca = new msal.ConfidentialClientApplication(msalConfig );
@@ -42,11 +43,55 @@ async function getUserDetails() {
   }
 }
 
-// ユーザー情報を取得して表示
-getUserDetails()
-  .then((users) => {
-    console.log(users);
-  })
-  .catch((error) => {
+// ユーザ情報を取得する関数
+async function getUserDetailsWithSearchQuery() {
+  const cca = new msal.ConfidentialClientApplication(msalConfig);
+  
+  const clientCredentialRequest = {
+    scopes: ["https://graph.microsoft.com/.default"], // API のパーミッション
+  };
+
+  try {
+    const response = await cca.acquireTokenByClientCredential(clientCredentialRequest);
+    
+    const client = graph.Client.init({
+      authProvider: (done) => {
+        done(null, response.accessToken);
+      },
+    });
+
+    // ユーザーの一覧を取得
+    //https://github.com/microsoftgraph/msgraph-sdk-javascript/blob/HEAD/docs/OtherAPIs.md#query
+    const user = await client.api("/users")
+    .header('ConsistencyLevel','eventual')
+    //.search('displayName:user')
+    .search('\"displayName:user\"')
+   // .query('$search="displayName:user"')
+    .get();
+    return user;
+
+  } catch (error) {
     console.log(error);
-  });
+    throw error;
+  }
+}
+
+// ユーザー情報を取得して表示
+// getUserDetails()
+//   .then((users) => {
+//     console.log(users);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+
+// Search Query を使ってユーザー情報を取得し、表示
+getUserDetailsWithSearchQuery()
+.then((users) => {
+  console.log(users);
+})
+.catch((error) => {
+  console.log(error);
+});
+
